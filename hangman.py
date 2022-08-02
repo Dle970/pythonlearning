a88ed06e3e81ee3658af8878a60efdb6fb6fd4c2
david <ledaiduong970@gmail.com>
2022-08-02 07:54:12 -0400

    commit(02/08/22_2)

--- hangman.py
+++ hangman.py
@@ -1,2 +1,142 @@
+import random
 
-words = 'ant badger bat bear deer dog'.split()
+HANGMANPICS = ['''
+
+
+
+ +---+
+
+ |   |
+
+     |
+
+     |
+
+     |
+
+     |
+
+=========''', '''
+
+
+
+ +---+
+
+ |   |
+
+ O   |
+
+     |
+
+     |
+
+     |
+
+=========''', '''
+
+
+
+ +---+
+
+ |   |
+
+ O   |
+
+ |   |
+
+     |
+
+     |
+
+=========''', '''
+
+
+
+ +---+
+
+ |   |
+
+ O   |
+
+/|   |
+
+     |
+
+     |
+
+  =========''', '''
+
+
+
+ +---+
+
+ |   |
+
+ O   |
+
+/|\  |
+
+     |
+
+     |
+
+=========''', '''
+
+
+
+ +---+
+
+ |   |
+
+ O   |
+
+/|\  |
+
+/    |
+
+     |
+
+=========''', '''
+
+
+
+ +---+
+
+ |   |
+
+ O   |
+
+/|\  |
+
+/ \  |
+
+     |
+
+=========''']
+
+words = 'ant badger bat bear deer dog random elephant'.split()
+
+def get_random_word(any_word_list):
+    return random.choice(any_word_list)
+
+def display_board(hangman_pics, missed_letters, correct_letters, secret_word):
+    print(hangman_pics[len(missed_letters)]) # assuming that you can only get 6 letters wrong?
+    print()
+
+    print("Missed Letters: ", end = " ")
+
+    for letter in missed_letters:
+        print(letter, end = " ")
+    print()
+    print()
+
+print("Hangman")
+
+missed_letters = ""
+correct_letters = ""
+secret_word = get_random_word(words)
+game_is_done = False
+
+while True:
+    display_board(HANGMANPICS, missed_letters, correct_letters, secret_word)
+    break
